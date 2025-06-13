8091b0c85d011184274118e364059608d58d9638
237-Arcan <alexevrance0@gmail.com>
2025-06-13 03:32:12 +0100

    Update __init__.py

--- data_integration/adapters/__init__.py
+++ data_integration/adapters/__init__.py
@@ -1,7 +1,7 @@
 # Rendre le module adapters importable
 from .base_adapter import BaseAdapter
 from .bet365_adapter import Bet365Adapter
-from .openapi_adapter import OpenAPIAdapter
+from .openapi_adapter import OpenapiAdapter
 from .soccerdata_adapter import SoccerdataAdapter
 from .github_adapter import GithubAdapter
 from .sports_betting_adapter import SportsBettingAdapter
